61603fb34ad105189041c7cce629c9e67820b594
Scott Lai <scottlai@cs.utexas.edu>
2023-05-12 00:11:49 -0500

    Adds detailed comments

--- backend/mongodb.js
+++ backend/mongodb.js
@@ -1,21 +1,31 @@
+/**
+ * imports modules
+ * - mongodb: Node.js driver for MongoDB
+ * - dotenv: use environment variables
+ */
 const mongodb = require('mongodb');
 const dotenv = require('dotenv');
 
+// loads the .env file holding the environment variables
 dotenv.config();
 
-// MongoDB connection URI
+// retrieves the MongoDB connection URI from .env
 const uri = process.env.MONGODB_URI;
 
+// creates a new MongoClient (a client connection to the MongoDB server)
 const client = new mongodb.MongoClient(uri, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
 
+// asynchronously connects to the database using connect()
 async function connect() {
   await client.connect();
   console.log('Connected to MongoDB!');
 }
 
+// calls the connect() function when the app starts
 connect();
 
+// exports the MongoClient for database operations throughout the app
 module.exports = client;
--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,9 +1,22 @@
-var express = require('express');
-var router = express.Router();
+/**
+ * imports modules
+ * - express: main framework
+ */
+const express = require('express');
 
-/* GET home page. */
+// creates a router object to handle HTTP requests
+const router = express.Router();
+
+/**
+ * Route handler for the GET method on the / endpoint
+ * @param req - incoming request object
+ * @param res - outgoing response object
+ * @param next - next middleware function in the request-response cycle
+ */
 router.get('/', function(req, res, next) {
+  // renders the view template index using the given parameters
   res.render('index', { title: 'Express' });
 });
 
+// exports the router object to be mounted to / in app.js
 module.exports = router;
