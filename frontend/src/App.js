e6b8c5c257d9950ca9a88f147e56951a8eb43b3d
Scott Lai <scottlai@cs.utexas.edu>
2023-05-11 22:24:42 -0500

    Gets courses from API

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import Axios from "axios";
+import Button from "@mui/material/Button";
 
 function App() {
+  const [courses, setCourses] = useState([]);
+
+  const getCourses = () => {
+    Axios.get("https://api.scottlai.tech/courses").then(
+      (response) => {
+        console.log(response);
+        setCourses(response.data);
+      }
+    );
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Button onClick={getCourses} variant="contained">
+        Get Courses
+      </Button>
     </div>
   );
 }
