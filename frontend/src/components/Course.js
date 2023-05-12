c644557187e81382713da79a7dc5851edbc44f54
Scott Lai <scottlai@cs.utexas.edu>
2023-05-12 02:26:57 -0500

    Extracts API calls, components, and utility functions from App.js

---
+++ frontend/src/components/Course.js
@@ -0,0 +1,48 @@
+import React from "react";
+import PropTypes from "prop-types"
+import MeetList from "./MeetList";
+
+const Course = ({ course }) => {
+    return (
+        <div>
+            <h1>
+                {course.courseNumber.department} {course.courseNumber.number}: {course.name} {course.nickname ? "(" + course.nickname + ")" : ""}
+            </h1>
+            <h2>{course.instructors.join(" & ")}</h2>
+            <h3>{course.term.semester} {course.term.year}</h3>
+            <MeetList meets={course.meets}></MeetList>
+            <h3>Misc.</h3>
+            <p>Grade: {course.grade}</p>
+            <p>Unique Number: {course.uniqueNumber}</p>
+        </div>
+    );
+};
+
+Course.propTypes = {
+    course: PropTypes.shape({
+        courseNumber: PropTypes.shape({
+          department: PropTypes.string.isRequired,
+          number: PropTypes.string.isRequired,
+        }).isRequired,
+        name: PropTypes.string.isRequired,
+        nickname: PropTypes.string,
+        instructors: PropTypes.arrayOf(PropTypes.string).isRequired,
+        term: PropTypes.shape({
+          semester: PropTypes.string.isRequired,
+          year: PropTypes.string.isRequired,
+        }).isRequired,
+        meets: PropTypes.arrayOf(PropTypes.shape({
+          days: PropTypes.arrayOf(PropTypes.string).isRequired,
+          startTime: PropTypes.number,
+          endTime: PropTypes.number,
+          room: PropTypes.shape({
+            building: PropTypes.string,
+            number: PropTypes.string,
+          }).isRequired,
+        })),
+        grade: PropTypes.string,
+        uniqueNumber: PropTypes.string.isRequired,
+      }).isRequired
+};
+
+export default Course;
