5233b90bd1486aebfce1abbc9ba5f128eb1a7a49
Scott Lai <scottlai@cs.utexas.edu>
2023-05-11 22:47:49 -0500

    Displays course info

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,11 @@
 import React, { useState } from "react";
 import Axios from "axios";
 import Button from "@mui/material/Button";
+import { format } from "date-fns";
+
+function formatTime(time) {
+  return format(new Date(time), "h:mm a");
+}
 
 function App() {
   const [courses, setCourses] = useState([]);
@@ -15,10 +20,43 @@ function App() {
   };
 
   return (
-    <div className="App">
+    <div>
       <Button onClick={getCourses} variant="contained">
         Get Courses
       </Button>
+      {courses?.map((course, i) => {
+        return (
+          <div key={i}>
+            <h1>
+              {course.courseNumber.department} {course.courseNumber.number}: {course.name} {course.nickname ? "(" + course.nickname + ")" : ""}
+            </h1>
+            <h2>{course.instructors.join(" & ")}</h2>
+            <h3>{course.term.semester} {course.term.year}</h3>
+            <h3>Meets</h3>
+            {course.meets?.map((meet, j) => {
+              var meetData = [meet.days.join("")];
+              if (meet.startTime) {
+                meetData.push(formatTime(meet.startTime) + " - " + formatTime(meet.endTime));
+              }
+              if (meet.room.building && meet.room.number) {
+                meetData.push(meet.room.building + " " + meet.room.number);
+              } else if (meet.room.building) {
+                meetData.push(meet.room.building);
+              }
+              return (
+                <div key={j}>
+                  <p>
+                    {meetData.join(", ")}
+                  </p>
+                </div>
+              );
+            })}
+            <h3>Misc.</h3>
+            <p>Grade: {course.grade}</p>
+            <p>Unique Number: {course.uniqueNumber}</p>
+          </div>
+        );
+      })}
     </div>
   );
 }
