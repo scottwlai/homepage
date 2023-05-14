2279b173c958f1e3f3190c73821faeb2249a087d
Scott Lai <scottlai@cs.utexas.edu>
2023-05-14 02:06:17 -0500

    Reduces repetition with map

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -1,10 +1,33 @@
-import { Toolbar, Container, Button, Typography, Grid } from "@mui/material";
-import { Link } from "react-router-dom";
 import React from "react";
+import { Link } from "react-router-dom";
+import { Toolbar, Container, Button, Typography, Grid } from "@mui/material";
 import LinkedInIcon from "@mui/icons-material/LinkedIn";
 import GitHubIcon from "@mui/icons-material/GitHub";
 import EmailIcon from "@mui/icons-material/Email";
 import PhoneIcon from "@mui/icons-material/Phone";
+
+const links = [
+  {
+    "to": "https://linkedin.com/in/scottwlai",
+    "icon": <LinkedInIcon sx={{ pr: 1 }} />,
+    "text": "LinkedIn"
+  },
+  {
+    "to": "https://github.com/scottwlai",
+    "icon": <GitHubIcon sx={{ pr: 1 }} />,
+    "text": "GitHub"
+  },
+  {
+    "to": "mailto:scottlai@cs.utexas.edu",
+    "icon": <EmailIcon sx={{ pr: 1 }} />,
+    "text": "scottlai@cs.utexas.edu"
+  },
+  {
+    "to": "tel:+17138205695",
+    "icon": <PhoneIcon sx={{ pr: 1 }} />,
+    "text": "(713) 820-5695"
+  }
+];
 
 const Footer = () => {
   return (
@@ -16,56 +39,22 @@ const Footer = () => {
       }}>
         <Container>
           <Grid container>
-            <Grid item xs={12} sm={6} md={3} sx={{
-              display: "flex",
-              justifyContent: "center",
-              "@media (max-width: 960px)": { justifyContent: "flex-start" }
-            }}>
-              <Button color="inherit" component={Link}
-                to="https://linkedin.com/in/scottwlai"
-                sx={{ px: 2 }}>
-                <LinkedInIcon sx={{ pr: 1 }} />
-                <Typography variant="button" textTransform="none">LinkedIn</Typography>
-              </Button>
-            </Grid>
-            <Grid item xs={12} sm={6} md={3} sx={{
-              display: "flex",
-              justifyContent: "center",
-              "@media (max-width: 960px)": { justifyContent: "flex-start" }
-            }}>
-              <Button color="inherit" component={Link}
-                to="https://github.com/scottwlai"
-                sx={{ px: 2 }}>
-                <GitHubIcon sx={{ pr: 1 }} />
-                <Typography variant="button" textTransform="none">GitHub</Typography>
-              </Button>
-            </Grid>
-            <Grid item xs={12} sm={6} md={3} sx={{
-              display: "flex",
-              justifyContent: "center",
-              "@media (max-width: 960px)": { justifyContent: "flex-start" }
-            }}>
-              <Button color="inherit" component={Link}
-                to="mailto:scottlai@cs.utexas.edu"
-                sx={{ px: 2 }}>
-                <EmailIcon sx={{ pr: 1 }} />
-                <Typography variant="button" textTransform="none">
-                  scottlai@cs.utexas.edu
-                </Typography>
-              </Button>
-            </Grid>
-            <Grid item xs={12} sm={6} md={3} sx={{
-              display: "flex",
-              justifyContent: "center",
-              "@media (max-width: 960px)": { justifyContent: "flex-start" }
-            }}>
-              <Button color="inherit" component={Link}
-                to="tel:+17138205695"
-                sx={{ px: 2 }}>
-                <PhoneIcon sx={{ pr: 1 }} />
-                <Typography variant="button" textTransform="none">(713) 820-5695</Typography>
-              </Button>
-            </Grid>
+            {links.map((link, index) => {
+              return (
+                <Grid key={index} item xs={12} sm={6} md={3} sx={{
+                  display: "flex",
+                  justifyContent: "center",
+                  "@media (max-width: 960px)": {justifyContent: "flex-start"}
+                }}>
+                  <Button color="inherit" component={Link} to={link.to} sx={{px: 2}}>
+                    {link.icon}
+                    <Typography variant="button" textTransform="none">
+                      {link.text}
+                    </Typography>
+                  </Button>
+                </Grid>
+              );
+            })}
           </Grid>
         </Container>
       </Toolbar>
