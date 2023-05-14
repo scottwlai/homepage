3d1db149b9be144bf1c3c43d1fec5b9ac0a78a73
Scott Lai <scottlai@cs.utexas.edu>
2023-05-14 01:34:01 -0500

    Formats footer with grid

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -1,32 +1,72 @@
-import { Toolbar, Container, Button, ButtonGroup, Typography } from "@mui/material";
+import { Toolbar, Container, Button, Typography, Grid } from "@mui/material";
 import { Link } from "react-router-dom";
 import React from "react";
+import LinkedInIcon from "@mui/icons-material/LinkedIn";
+import GitHubIcon from "@mui/icons-material/GitHub";
+import EmailIcon from "@mui/icons-material/Email";
+import PhoneIcon from "@mui/icons-material/Phone";
 
 const Footer = () => {
   return (
     <footer>
       <Toolbar component="nav" sx={{
         bgcolor: "#1976d2",
-        color: "#fff"
+        color: "#fff",
+        py: 2
       }}>
         <Container>
-          <ButtonGroup variant="text" aria-label="contact buttons" sx={{
-            "& .MuiButtonGroup-grouped:not(:last-child)": { borderRight: "none" },
-            display: "flex",
-            flexDirection: "row",
-            justifyContent: "space-evenly"
-          }}>
-            <Button color="inherit" component={Link} to="https://linkedin.com/in/scottwlai">
-              <Typography variant="button" textTransform="none">LinkedIn</Typography>
-            </Button>
-            <Button color="inherit" component={Link} to="https://github.com/scottwlai">
-              <Typography variant="button" textTransform="none">GitHub</Typography>
-            </Button>
-            <Button color="inherit" component={Link} to="mailto:scottlai@cs.utexas.edu">
-              <Typography variant="button" textTransform="none">scottlai@cs.utexas.edu</Typography>
-            </Button>
-            <Button color="inherit" component={Link} to="tel:+17138205695">(713) 820-5695</Button>
-          </ButtonGroup>
+          <Grid container>
+            <Grid item xs={12} sm={6} md={3} sx={{
+              display: "flex",
+              justifyContent: "center",
+              "@media (max-width: 960px)": { justifyContent: "flex-start" }
+            }}>
+              <Button color="inherit" component={Link}
+                to="https://linkedin.com/in/scottwlai"
+                sx={{ px: 2 }}>
+                <LinkedInIcon sx={{ pr: 1 }} />
+                <Typography variant="button" textTransform="none">LinkedIn</Typography>
+              </Button>
+            </Grid>
+            <Grid item xs={12} sm={6} md={3} sx={{
+              display: "flex",
+              justifyContent: "center",
+              "@media (max-width: 960px)": { justifyContent: "flex-start" }
+            }}>
+              <Button color="inherit" component={Link}
+                to="https://github.com/scottwlai"
+                sx={{ px: 2 }}>
+                <GitHubIcon sx={{ pr: 1 }} />
+                <Typography variant="button" textTransform="none">GitHub</Typography>
+              </Button>
+            </Grid>
+            <Grid item xs={12} sm={6} md={3} sx={{
+              display: "flex",
+              justifyContent: "center",
+              "@media (max-width: 960px)": { justifyContent: "flex-start" }
+            }}>
+              <Button color="inherit" component={Link}
+                to="mailto:scottlai@cs.utexas.edu"
+                sx={{ px: 2 }}>
+                <EmailIcon sx={{ pr: 1 }} />
+                <Typography variant="button" textTransform="none">
+                  scottlai@cs.utexas.edu
+                </Typography>
+              </Button>
+            </Grid>
+            <Grid item xs={12} sm={6} md={3} sx={{
+              display: "flex",
+              justifyContent: "center",
+              "@media (max-width: 960px)": { justifyContent: "flex-start" }
+            }}>
+              <Button color="inherit" component={Link}
+                to="tel:+17138205695"
+                sx={{ px: 2 }}>
+                <PhoneIcon sx={{ pr: 1 }} />
+                <Typography variant="button" textTransform="none">(713) 820-5695</Typography>
+              </Button>
+            </Grid>
+          </Grid>
         </Container>
       </Toolbar>
     </footer >
