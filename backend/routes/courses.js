8ef7fa7f1c47add0e1dfcbea2deff2a96c627cc3
Scott Lai <scottlai@cs.utexas.edu>
2023-05-19 00:40:30 -0500

    Whitelists domain names
    Closes #8

--- backend/routes/courses.js
+++ backend/routes/courses.js
@@ -2,12 +2,31 @@
  * imports modules
  * - express: main framework
  * - mongodb: client connection to MongoDB
+ * - cors: cross-origin resource sharing middleware
  */
 const express = require('express');
 const mongodb = require('../mongodb');
+const cors = require('cors');
 
 // creates a router object to handle HTTP requests
 const router = express.Router();
+
+// creates a list of whitelisted origins
+const whitelist = [
+  'https://scottlai.tech',
+  'https://www.scottlai.tech'
+];
+
+// allows the server to serve origins in the whitelist only
+corsOptions = {
+  origin: (origin, callback) => {
+    if (whitelist.indexOf(origin) !== -1) {
+      callback(null, true);
+    } else {
+      callback(new Error('Now allowed by CORS'));
+    }
+  }
+};
 
 /**
  * Route handler for the GET method on the /courses endpoint
@@ -15,7 +34,7 @@ const router = express.Router();
  * @param res - outgoing response object
  * @param next - next middleware function in the request-response cycle
  */
-router.get('/', async(req, res, next) => {
+router.get('/', cors(corsOptions), async(req, res, next) => {
   // connects to the Homepage database in the MongoDB instance
   const db = mongodb.db('Homepage');
   // selects the Courses collection in the Homepage database
