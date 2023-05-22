54cf6500bbbb7c69194b5a2d35cb92aed3ef145a
Scott Lai <scottlai@cs.utexas.edu>
2023-05-21 23:03:07 -0500

    Replaces schools array with UT object

--- frontend/src/components/Education.js
+++ frontend/src/components/Education.js
@@ -2,30 +2,49 @@ import React, {
   useState, useEffect
 } from "react"
 import {
-  Box, Card, CardContent, Grid, List, ListItem, ListItemIcon, ListItemText,
+  Box,
   Table, TableBody, TableCell, TableContainer, TableHead, TablePagination, TableRow, Typography
 } from "@mui/material";
 import SchoolIcon from '@mui/icons-material/School';
 import MilitaryTechIcon from '@mui/icons-material/MilitaryTech';
-import CalendarMonthIcon from '@mui/icons-material/CalendarMonth';
-import PlaceIcon from '@mui/icons-material/Place';
 import {
   getCourses
 } from "../api";
+import CustomCard from "./CustomCard.js";
 
-const schools = [
-  {
-    "name": "University of Texas at Austin",
-    "degrees": [
-      "B.S. Computer Science",
-      "Business Minor"
-    ],
-    "gpa": 3.6542,
-    "startDate": "Aug 2020",
-    "endDate": "May 2024",
-    "location": "Austin, TX"
-  }
-];
+const ut = {
+  "image": null,
+  "title": "University of Texas at Austin",
+  "subtitles": [
+    {
+      "subtitle": "B.S. Computer Science",
+      "subtitleIcon": <SchoolIcon sx={{
+        fontSize: "1.375rem",
+        color: "#333"
+      }} />
+    },
+    {
+      "subtitle": "Business Minor",
+      "subtitleIcon": <SchoolIcon sx={{
+        fontSize: "1.375rem",
+        color: "#333"
+      }} />
+    },
+    {
+      "subtitle": "GPA: 3.6542",
+      "subtitleIcon": <MilitaryTechIcon sx={{
+        fontSize: "1.375rem",
+        color: "#333"
+      }} />
+    }
+  ],
+  "startDate": "Aug 2020",
+  "endDate": "May 2024",
+  "location": "Austin, TX",
+  "bullets": null,
+  "columns": 1,
+  "tags": false
+}
 
 const Education = () => {
   const [ courses, setCourses ] = useState([]);
@@ -84,141 +103,53 @@ const Education = () => {
       }}>
         Education
       </Typography>
-      <Grid container spacing={"2.5rem"}>
-        {schools.map((school, index) => {
-          return (
-            <Grid item key={index} xs={12}>
-              <Card component="section" sx={{
-                display: "flex",
-                flexDirection: "row",
-                borderRadius: "1.5rem",
-                height: "100%"
-              }}>
-                <CardContent sx={{
-                  p: 6
-                }}>
-                  <Typography variant="h3">
-                    {school.name}
-                  </Typography>
-                  <List>
-                    {school.degrees.map((degree, j) => {
-                      return (
-                        <ListItem key={j} sx={{
-                          p: 0
-                        }}>
-                          <ListItemIcon sx={{
-                            display: "flex",
-                            justifyContent: "center",
-                            minWidth: "3rem"
-                          }}>
-                            <SchoolIcon sx={{
-                              fontSize: "1.375rem",
-                              color: "#333"
-                            }} />
-                          </ListItemIcon>
-                          <ListItemText primary={degree} />
-                        </ListItem>
-                      );
-                    })}
-                    <ListItem sx={{
-                      p: 0
-                    }}>
-                      <ListItemIcon sx={{
-                        display: "flex",
-                        justifyContent: "center",
-                        minWidth: "3rem"
-                      }}>
-                        <MilitaryTechIcon sx={{
-                          fontSize: "1.375rem",
-                          color: "#333"
-                        }} />
-                      </ListItemIcon>
-                      <ListItemText primary={school.gpa} />
-                    </ListItem>
-                    <ListItem sx={{
-                      p: 0
-                    }}>
-                      <ListItemIcon sx={{
-                        display: "flex",
-                        justifyContent: "center",
-                        minWidth: "3rem"
-                      }}>
-                        <CalendarMonthIcon sx={{
-                          fontSize: "1.375rem",
-                          color: "#333"
-                        }} />
-                      </ListItemIcon>
-                      <ListItemText primary={school.startDate + " - " + school.endDate} />
-                    </ListItem>
-                    <ListItem sx={{
-                      p: 0
-                    }}>
-                      <ListItemIcon sx={{
-                        display: "flex",
-                        justifyContent: "center",
-                        minWidth: "3rem"
-                      }}>
-                        <PlaceIcon sx={{
-                          fontSize: "1.375rem",
-                          color: "#333"
-                        }} />
-                      </ListItemIcon>
-                      <ListItemText primary={school.location} />
-                    </ListItem>
-                  </List>
-                  <Typography variant="h4">
-                    Coursework
-                  </Typography>
-                  <TableContainer>
-                    <Table aria-label="simple table" sx={{
-                      minWidth: "100%"
-                    }}>
-                      <TableHead>
-                        <TableRow>
-                          <TableCell>Name</TableCell>
-                          <TableCell>Instructor(s)</TableCell>
-                          <TableCell>Term</TableCell>
-                          <TableCell>Grade</TableCell>
-                        </TableRow>
-                      </TableHead>
-                      <TableBody>
-                        {courses.map((course, j) => (
-                          <TableRow
-                            key={j}
-                            sx={{
-                              "&:last-child td, &:last-child th": {
-                                border: 0
-                              }
-                            }}
-                          >
-                            <TableCell component="th" scope="row">
-                              {course.courseNumber.department + " "}
-                              {course.courseNumber.number + ": "}
-                              {course.nickname ? course.nickname : course.name}
-                            </TableCell>
-                            <TableCell>{course.instructors[0]}</TableCell>
-                            <TableCell>{course.term.semester + " " + course.term.year}</TableCell>
-                            <TableCell>{course.grade}</TableCell>
-                          </TableRow>
-                        ))}
-                      </TableBody>
-                    </Table>
-                  </TableContainer>
-                  <TablePagination
-                    component="div"
-                    count={count}
-                    page={currentPage - 1}
-                    onPageChange={handlePageChange}
-                    rowsPerPage={perPage}
-                    onRowsPerPageChange={handlePerPageChange}
-                    rowsPerPageOptions={[ 10, 20, 30 ]}
-                  />
-                </CardContent>
-              </Card>
-            </Grid>
-          );
-        })}
-      </Grid>
+      <CustomCard content={ut} flexDirection="column">
+        <Typography variant="h4">
+          Coursework
+        </Typography>
+        <TableContainer>
+          <Table aria-label="simple table" sx={{
+            minWidth: "100%"
+          }}>
+            <TableHead>
+              <TableRow>
+                <TableCell>Name</TableCell>
+                <TableCell>Instructor</TableCell>
+                <TableCell>Term</TableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {courses.map((course, index) => (
+                <TableRow
+                  key={index}
+                  sx={{
+                    "&:last-child td, &:last-child th": {
+                      border: 0
+                    }
+                  }}
+                >
+                  <TableCell component="th" scope="row">
+                    {course.courseNumber.department + " "}
+                    {course.courseNumber.number + ": "}
+                    {course.nickname ? course.nickname : course.name}
+                  </TableCell>
+                  <TableCell>{course.instructors[0]}</TableCell>
+                  <TableCell>{course.term.semester + " " + course.term.year}</TableCell>
+                </TableRow>
+              ))}
+            </TableBody>
+          </Table>
+        </TableContainer>
+        <TablePagination
+          component="div"
+          count={count}
+          page={currentPage - 1}
+          onPageChange={handlePageChange}
+          rowsPerPage={perPage}
+          onRowsPerPageChange={handlePerPageChange}
+          rowsPerPageOptions={[ 10, 20, 30 ]}
+        />
+      </CustomCard>
     </Box>
   );
 }
