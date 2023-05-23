ea98207a531419f5074ca056ddc9d6d45d7cae09
Scott Lai <scottlai@cs.utexas.edu>
2023-05-22 21:26:43 -0500

    Readds courses route under portfolio

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,6 +4,7 @@ import {
 } from "react-router-dom";
 import Home from "./pages/Home";
 import Portfolio from "./pages/Portfolio";
+import Courses from "./pages/Courses";
 import Interests from "./pages/Interests";
 import Demo from "./pages/Demo";
 import NoMatch from "./pages/NoMatch";
@@ -13,6 +14,7 @@ const App = () => {
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/portfolio" element={<Portfolio />} />
+      <Route path="/portfolio/courses" element={<Courses />} />
       <Route path="/interests" element={<Interests />} />
       <Route path="/demo" element={<Demo />} />
       <Route path="/*" element={<NoMatch />} />
