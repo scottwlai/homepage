5988ebb98100e451357b4333ae43b5993d026787
Scott Lai <scottlai@cs.utexas.edu>
2024-03-13 23:29:50 -0500

    Fixes spacing
    Closes #42

--- frontend/src/components/Hobbies.js
+++ frontend/src/components/Hobbies.js
@@ -1,12 +1,17 @@
 import React from "react";
 import Wrapper from "./common/Wrapper";
+import Section from "./home/Section";
+import SectionText from "./home/SectionText";
 import SectionImage from "./home/SectionImage";
 import {
-  Typography
+  Typography,
+  useTheme
 } from "@mui/material";
 import Title from "./common/Title";
 
 const Hobbies = () => {
+  const theme = useTheme();
+
   return (
     <main>
       <Wrapper>
@@ -23,60 +28,66 @@ const Hobbies = () => {
           alt="Kickboxing Club"
           width={500}
           sx={{
-            borderRadius: "5%"
+            borderRadius: "5%",
+            m: theme.typography.h1.fontSize
           }}
         />
-        <Typography paragraph align="center" sx={{
+        <Typography variant="body2" paragraph align="center" sx={{
           textWrap: "balance",
           maxWidth: "60%"
         }}>
           I practice mixed martial arts every week with the Texas
           Striking Laboratory, a kickboxing club at UT.
         </Typography>
-      </Wrapper>
-      <Wrapper>
-        <Typography paragraph align="center" sx={{
-          textWrap: "balance",
-          maxWidth: "60%"
-        }}>
-          I&apos;ve been learning Muay Thai for a year now and have
-          begun sparring.
-        </Typography>
-        <SectionImage
-          src="https://cs.utexas.edu/~scottlai/images/sparring.jpg"
-          alt="sparring practice"
-          width={500}
-          sx={{
-            borderRadius: "5%"
-          }}
-        />
-        <Typography paragraph align="center" sx={{
-          textWrap: "balance",
-          maxWidth: "60%"
-        }}>
-          Aside from self-defense, I&apos;ve learned a great deal of
-          discipline, patience, and respect from my practice.
-        </Typography>
-      </Wrapper>
-      <Wrapper>
-        <Typography paragraph align="center" sx={{
-          textWrap: "balance",
-          maxWidth: "60%"
-        }}>
-          This past semester, I became an officer in the club,
-          because I wanted to help provide a safe and welcoming
-          environment for anyone interested in learning martial
-          arts.
-        </Typography>
-        <SectionImage
-          src="https://cs.utexas.edu/~scottlai/images/box.jpg"
-          alt="shadowboxing practice"
-          width={500}
-          sx={{
-            borderRadius: "5%"
-          }}
-        />
-        <Typography paragraph align="center" sx={{
+        <Section alignRight>
+          <SectionImage
+            src="https://cs.utexas.edu/~scottlai/images/sparring.jpg"
+            alt="sparring practice"
+            width={360}
+            sx={{
+              display: "flex",
+              borderRadius: "5%",
+              gridArea: "img"
+            }}
+          />
+          <SectionText alignRight>
+            <Typography variant="body2" paragraph sx={{
+              textWrap: "balance"
+            }}>
+              I&apos;ve been learning Muay Thai for a year now and have
+              begun sparring.
+            </Typography>
+            <Typography variant="body2" paragraph sx={{
+              textWrap: "balance"
+            }}>
+              Aside from self-defense, I&apos;ve learned a great deal of
+              discipline, patience, and respect from my practice.
+            </Typography>
+          </SectionText>
+        </Section>
+        <Section>
+          <SectionImage
+            src="https://cs.utexas.edu/~scottlai/images/box.jpg"
+            alt="shadowboxing practice"
+            width={360}
+            sx={{
+              display: "flex",
+              borderRadius: "5%",
+              gridArea: "img"
+            }}
+          />
+          <SectionText>
+            <Typography variant="body2" paragraph sx={{
+              textWrap: "balance"
+            }}>
+              This past semester, I became an officer in the club,
+              because I wanted to help provide a safe and welcoming
+              environment for anyone interested in learning martial
+              arts.
+            </Typography>
+          </SectionText>
+        </Section>
+        <Typography variant="body2" paragraph align="center" sx={{
           textWrap: "balance",
           maxWidth: "60%"
         }}>
@@ -88,60 +99,73 @@ const Hobbies = () => {
         <Typography gutterBottom variant="h2">
           I color grade
         </Typography>
-      </Wrapper>
-      <Wrapper>
-        <Typography paragraph align="center" sx={{
-          textWrap: "balance",
-          maxWidth: "60%"
-        }}>
-          I enjoy producing digital media, and I&apos;m happy to use
-          my skills in service to the club.
-        </Typography>
-        <SectionImage
-          src="https://cs.utexas.edu/~scottlai/images/james.jpg"
-          alt="My friend James sparring Coach Chartouni"
-          width={500}
-          sx={{
-            borderRadius: "5%"
-          }}
-        />
-        <Typography paragraph align="center" sx={{
-          textWrap: "balance",
-          maxWidth: "60%"
-        }}>
-          I color grade the photos I take in Photoshop and edit my
-          videos in Premiere Pro.
-        </Typography>
+        <Section alignRight>
+          <SectionImage
+            src="https://cs.utexas.edu/~scottlai/images/james.jpg"
+            alt="My friend James sparring Coach Chartouni"
+            width={360}
+            sx={{
+              display: "flex",
+              borderRadius: "5%",
+              gridArea: "img"
+            }}
+          />
+          <SectionText alignRight>
+            <Typography variant="body2" paragraph sx={{
+              textWrap: "balance"
+            }}>
+              I enjoy producing digital media, and I&apos;m happy to use
+              my skills in service to the club.
+            </Typography>
+            <Typography variant="body2" paragraph sx={{
+              textWrap: "balance"
+            }}>
+              I color grade the photos I take in Photoshop and edit my
+              videos in Premiere Pro.
+            </Typography>
+          </SectionText>
+        </Section>
       </Wrapper>
       <Wrapper>
         <Typography gutterBottom variant="h2">
           I edit videos
         </Typography>
-        <Typography paragraph align="center" sx={{
-          textWrap: "balance",
-          maxWidth: "60%"
-        }}>
-          While I&apos;m new to photo editing, I&apos;ve been making
-          videos since 2015.
-        </Typography>
-        <iframe
-          width="338"
-          height="600"
-          src="https://youtube.com/embed/sXkTO50NP8o?si=lUC9vJQME0-19wcl"
-          title="YouTube video player"
-          frameborder="0"
-          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-          allowfullscreen>
-        </iframe>
-        <Typography paragraph align="center" sx={{
-          textWrap: "balance",
-          maxWidth: "60%"
-        }}>
-          Since then, I&apos;ve been expanding my skillset and have
-          taken interest in keyframing and animated typography.
-        </Typography>
-      </Wrapper>
-      <Wrapper>
+        <Section>
+          <iframe
+            width="338"
+            height="600"
+            src="https://youtube.com/embed/sXkTO50NP8o?si=lUC9vJQME0-19wcl"
+            title="YouTube video player"
+            frameborder="0"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+            allowfullscreen
+            style={{
+              display: "flex",
+              gridArea: "img"
+            }}
+          >
+          </iframe>
+          <SectionText>
+            <Typography variant="body2" paragraph sx={{
+              textWrap: "balance"
+            }}>
+              While I&apos;m new to photo editing, I&apos;ve been making
+              ideos since 2015.
+            </Typography>
+            <Typography variant="body2" paragraph sx={{
+              textWrap: "balance"
+            }}>
+              Since then, I&apos;ve been expanding my skillset and have
+              taken interest in keyframing and animated typography.
+            </Typography>
+            <Typography variant="body2" paragraph sx={{
+              textWrap: "balance"
+            }}>
+              I like to edit funny clips of me and my friends, and I
+              occasionally upload to my YouTube channel.
+            </Typography>
+          </SectionText>
+        </Section>
         <iframe
           width="560"
           height="315"
@@ -149,19 +173,20 @@ const Hobbies = () => {
           title="YouTube video player"
           frameborder="0"
           allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-          allowfullscreen>
+          allowfullscreen
+        >
         </iframe>
-        <Typography paragraph align="center" sx={{
-          textWrap: "balance",
-          maxWidth: "60%"
-        }}>
-          I like to edit funny clips of me and my friends, and I
-          occasionally upload to my YouTube channel.
-        </Typography>
       </Wrapper>
       <Wrapper>
         <Typography gutterBottom variant="h2">
           I play piano
+        </Typography>
+        <Typography variant="body2" paragraph align="center" sx={{
+          textWrap: "balance",
+          maxWidth: "60%"
+        }}>
+          Besides my edits, I also upload my progress in learning
+          piano, though I keep these videos unlisted.
         </Typography>
         <iframe
           width="560"
@@ -170,18 +195,12 @@ const Hobbies = () => {
           title="YouTube video player"
           frameborder="0"
           allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-          allowfullscreen>
+          allowfullscreen
+          style={{
+            margin: theme.typography.h1.fontSize
+          }}>
         </iframe>
-        <Typography paragraph align="center" sx={{
-          textWrap: "balance",
-          maxWidth: "60%"
-        }}>
-          Besides my edits, I also upload my progress in learning
-          piano, though I keep these videos unlisted.
-        </Typography>
-      </Wrapper>
-      <Wrapper>
-        <Typography paragraph align="center" sx={{
+        <Typography variant="body2" paragraph align="center" sx={{
           textWrap: "balance",
           maxWidth: "60%"
         }}>
@@ -196,9 +215,12 @@ const Hobbies = () => {
           title="YouTube video player"
           frameborder="0"
           allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-          allowfullscreen>
+          allowfullscreen
+          style={{
+            margin: theme.typography.h1.fontSize
+          }}>
         </iframe>
-        <Typography paragraph align="center" sx={{
+        <Typography variant="body2" paragraph align="center" sx={{
           textWrap: "balance",
           maxWidth: "60%"
         }}>
